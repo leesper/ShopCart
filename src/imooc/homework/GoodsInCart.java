2d95a68dbbcee1dac8f99e42d61fd49edfa63bc1
李科君 <likejun@likejundeMacBook-Pro.local>
2019-02-17 08:57:54 +0800

    generage GoodsInCart

--- src/imooc/homework/GoodsInCart.java
+++ src/imooc/homework/GoodsInCart.java
@@ -1,5 +1,30 @@
 package imooc.homework;
 
 public class GoodsInCart {
+	private Goods goods;
+	private int num;
+	
+	public GoodsInCart(Goods goods, int num) {
+		super();
+		this.goods = goods;
+		this.num = num;
+	}
 
+	public Goods getGoods() {
+		return goods;
+	}
+
+	public void setGoods(Goods goods) {
+		this.goods = goods;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	
 }
