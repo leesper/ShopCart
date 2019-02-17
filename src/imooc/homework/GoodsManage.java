0cfbb54a3da25887d853678c2a75327b815ad65c
李科君 <likejun@likejundeMacBook-Pro.local>
2019-02-17 08:52:43 +0800

    generage GoodsManage

--- src/imooc/homework/GoodsManage.java
+++ src/imooc/homework/GoodsManage.java
@@ -1,5 +1,24 @@
 package imooc.homework;
 
-public class GoodsManage {
+import java.util.HashSet;
+import java.util.Set;
 
+public class GoodsManage {
+	private Set<Goods> goodsSet;
+	
+	public GoodsManage() {
+		goodsSet = new HashSet<>();
+	}
+
+	public Set<Goods> getGoodsSet() {
+		return goodsSet;
+	}
+
+	public void setGoodsSet(Set<Goods> goodsSet) {
+		this.goodsSet = goodsSet;
+	}
+	
+	public void importGoods() {}
+	
+	public void displayAllGoods() {}
 }
